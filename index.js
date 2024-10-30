3e0813e12d1111b9fc370f4792d688203b8450b5
Elchonon <ekn400@gmail.com>
2024-10-30 18:58:36 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const myTestDrivers = ['Bobby', 'Steve','Bobby'];
+
+function findMatching(arrayDrivers,filterString){
+    const result = arrayDrivers.filter((driver) => driver.toLowerCase() == filterString.toLowerCase())
+    return result;
+}
+
+function fuzzyMatch(arrayDrivers,filterString){
+    const result = arrayDrivers.filter((driver) => driver.toLowerCase().startsWith(filterString.toLowerCase()))
+    return result;
+}
+
+function matchName(arrayDriverObjs,filterString){
+    const result = arrayDriverObjs.filter((driverObj) => driverObj["name"].toLowerCase() == filterString.toLowerCase())
+    return result;
+}
+//findMatching(myTestDrivers,"Bobby")
